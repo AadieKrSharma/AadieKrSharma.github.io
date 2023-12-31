44ae96338db2d51aa20cdcf940d7b3fe7c96adfd
Aaditya Kumar Sharma <sharma.kumaraaditya.5899@gmail.com>
2023-12-31 22:05:09 +0530

    Rectifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import './App.css'
 import Navbar from './components/navbar/navbar'
+import {useEffect} from 'react'
+
+
 
 function App() {
+
+  useEffect(()=>{
+    document.title = process.env.OWNER_NAME + " : PortFolio" || "PortFolio"
+  },[])
 
   return (
     <>
