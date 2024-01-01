3ef127a9206b9b8884a416f5df8a800a65ce3d84
Aaditya Kumar Sharma <sharma.kumaraaditya.5899@gmail.com>
2024-01-01 16:33:13 +0530

    Redesigns Implemented

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,14 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./navbar.module.css";
+import * as data from './nav.links.json';
+import pageLogo from "../../assets/channel watermark.png";
+const linksString = JSON.stringify(data);
+const links = JSON.parse(linksString).links;
+
+type Link = {
+  label: string;
+  href: string;
+}
+
+
+
+const Links: React.FC<{ links: Link[] }> = ({ links }) => {
+  return (
+    <>
+      <div className={`${styles['links-container']}`}>
+        {links.map((link: Link) => {
+          return (
+            <div key={link.href} className={styles['link']}>
+              <a href={link.href}>{link.label}</a>
+            </div>
+          )
+        })}
+      </div>
+    </>
+  );
+}
+
+
+const FloatingMenu: React.FC = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = ()=>{
+    setIsActive(current => !current);
+  }
+  return (
+    <>
+      <div className={isActive ? `${styles['floatingMenu']}`:`${styles['hideMenu']}`}>
+        <Links links={links} />
+      </div>
+    </>
+  );
+}
 
 const Navbar: React.FC = () => {
+  const pageName = process.env.OWNER_NAME?.split(" ")[0];
   return (
-    <nav className={styles.navBar}>
-      <div className={styles['logo-container']}>
-        {/* <span>{process.env.OWNER_NAME}</span> */}
-        <span>Logo</span>
-      </div>
-    </nav>
+    <>
+      <nav className={styles.navBar}>
+        <div className={styles['logo-container']}>
+          <img src={pageLogo} className={styles['pageLogo']} />
+          <span className={styles['shortName']}>{pageName}</span>
+          <span className={styles['longName']}>{process.env.OWNER_NAME}</span>
+        </div>
+        <Links links={links} />
+      </nav>
+      <FloatingMenu />
+    </>
   )
 }
 
